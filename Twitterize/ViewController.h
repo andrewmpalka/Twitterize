e6519aae0e60ac08dfe3ff6ea586e988fea56a98
Andy Palka <andrewmpalka@gmail.com>
2016-01-13 13:43:20 -0600

    Initial Commit

---
+++ Twitterize/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Twitterize
+//
+//  Created by Andrew Palka on 1/13/16.
+//  Copyright © 2016 Andrew Palka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Twitterize/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Twitterize
+//
+//  Created by Andrew Palka on 1/13/16.
+//  Copyright © 2016 Andrew Palka. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
